c4071c40908cf7c8593d4dae8be08ee94ea79b67
jonathanfdez77 <jonathanfdez62@gmail.com>
2024-08-04 21:38:01 -0500

    Components and Pages

---
+++ frontend/src/components/corousel/CarouselItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Carousel.scss"
+
+const CarouselItem = () => {
+    return(
+        <div>
+            CarouselItem
+        </div>
+    )
+}
+
+export default CarouselItem;
