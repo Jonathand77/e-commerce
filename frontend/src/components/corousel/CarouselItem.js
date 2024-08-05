9936ea05048e78bf3ade9b7800bab28d8c98e629
jonathanfdez77 <jonathanfdez62@gmail.com>
2024-08-04 21:38:25 -0500

    Components and Pages

--- frontend/src/components/corousel/CarouselItem.js
+++ frontend/src/components/corousel/CarouselItem.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./Carousel.scss"
+import { Link } from 'react-router-dom';
 
-const CarouselItem = () => {
+const CarouselItem = ({url, name, price, description}) => {
     return(
-        <div>
-            CarouselItem
+        <div className='carouselItem'>
+            <Link to="/product-details">
+                <img className='product--image' src={url} alt='product'></img>
+                <p className='price'>
+                    {`$${price}`}
+                </p>
+                <h4>{name}</h4>
+                <p className='--mb'>{description}</p>
+            </Link>
+            <button className='--btn --btn-primary'>Add to Cart</button>
         </div>
     )
 }
